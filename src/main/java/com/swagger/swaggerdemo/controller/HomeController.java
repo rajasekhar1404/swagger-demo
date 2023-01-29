267eb55b837e554767b3d73011c1c4b15cecb304
rajasekhar1404 <mrrajasekhar09@gmail.com>
2023-01-29 21:38:03 +0530

    Updates

--- src/main/java/com/swagger/swaggerdemo/controller/HomeController.java
+++ src/main/java/com/swagger/swaggerdemo/controller/HomeController.java
@@ -1,2 +1,16 @@
-package com.swagger.swaggerdemo.controller;public class HomeController {
+package com.swagger.swaggerdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/getHome")
+    public String getHome() {
+        return "Welcome to home..!";
+    }
+
 }
